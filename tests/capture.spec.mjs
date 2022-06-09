6fb41c7454ac65d102d75628f3cf905025da169e
Masahiro Hayashi <mhayashi@mshackers.co.jp>
2022-06-10 08:43:31 +0900

    テスト修正

--- tests/capture.spec.mjs
+++ tests/capture.spec.mjs
@@ -7,21 +7,21 @@ import { test, expect } from "@playwright/test";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-test("all page", async ({ page }, testInfo) => {
-  const sitemap = await fs.readFile(path.resolve(__dirname, "../public/sitemap.xml"));
-  const $ = cheerio.load(sitemap);
-  const urls = $("url > loc")
-    .toArray()
-    .map((el) => $(el).text());
-  urls.sort((a, b) => a.localeCompare(b));
+const sitemap = await fs.readFile(path.resolve(__dirname, "../public/sitemap.xml"));
+const $ = cheerio.load(sitemap);
+const urls = $("url > loc")
+  .toArray()
+  .map((el) => $(el).text());
+urls.sort((a, b) => a.localeCompare(b));
 
-  let count = 0;
-  for (const url of urls) {
-    count++;
+test("all page", async ({ page }, testInfo) => {
+  for (let index = 0; index < urls.length; index++) {
+    const url = urls[index];
+
     await page.goto("http://localhost:1313" + url);
     const dir = testInfo.outputDir;
     await fs.mkdir(dir, { recursive: true });
-    const filename = `${`${count}`.padStart(4, "0")}_root${decodeURIComponent(url)
+    const filename = `${`${index + 1}`.padStart(4, "0")}_root${decodeURIComponent(url)
       .replaceAll("/", "_")
       .slice(0, -1)}.png`;
     await page.screenshot({ fullPage: true, path: path.join(dir, filename) });
